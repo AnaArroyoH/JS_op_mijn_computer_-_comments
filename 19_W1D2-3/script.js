d0355f85c30a4101c5596741cecf623770869a9e
AnaArroyoH <75263257+AnaArroyoH@users.noreply.github.com>
2021-05-22 16:06:14 +0200

    Add files via upload

---
+++ 19_W1D2-3/script.js
@@ -0,0 +1,14 @@
+console.log("Hallo Winc Academy studenten");
+//Dit is een grote som
+let sum = 1230941 * 1823794;
+console.log(sum);
+//Hierboven zie je de uitkomst van de som omdat de comment bovenaan alleen voor die ene regel geldt
+let division = 637263 / 54;
+console.log(division);
+
+/*let sum = 1230941 * 1823794;
+console.log(sum);
+let division = 637263 / 54;
+console.log(division); */
+
+// Hierboven gaat het om een block comment dus de hele code staat nu uit en zie je niet in de console
